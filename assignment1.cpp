302910c6d73a2c5b957621db4cd66c1efc096e8a
Seif Zakaria Mohamed Ellaban <62963968+seifzellaban@users.noreply.github.com>
2023-02-28 21:37:05 +0200

    Update assignment1.cpp

--- assignment1.cpp
+++ assignment1.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main() {
-     float num1, num2;
-    std::cout << "Please enter two integers: ";
-    std::cin >> num1 >> num2;
-
-    std::cout << "Sum: " << num1 + num2 << std::endl;
-    std::cout << "Difference: " << num1 - num2 << std::endl;
-    std::cout << "Product: " << num1 * num2 << std::endl;
-    std::cout << "Quotient: " << num1 / num2 << std::endl;
-    std::cout << "Remainder: " << int(num1) % int(num2) << std::endl;
+    float num1, num2;
+    cout << "Please enter the first number: ";
+    cin >> num1;
+    cout << "Please enter the second number: ";
+    cin >> num2;
+    cout << "Sum: " << num1 + num2 << std::endl;
+    cout << "Difference: " << num1 - num2 << std::endl;
+    cout << "Product: " << num1 * num2 << std::endl;
+    cout << "Quotient: " << num1 / num2 << std::endl;
+    cout << "Remainder: " << int(num1) % int(num2) << std::endl;
     system("pause");
     return 0;
 }
