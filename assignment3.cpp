727ad4bf3fefb31212df96c65041f49ebd1421d0
Seif Zakaria Mohamed Ellaban <62963968+seifzellaban@users.noreply.github.com>
2023-03-03 20:31:28 +0200

    Update assignment3.cpp

--- assignment3.cpp
+++ assignment3.cpp
@@ -9,13 +9,13 @@ int main(){
     cout << "3.  The value of j3 is 0 because 2.4 * i = 240.0 and 240.0 / 3 = 80.0, which has a remainder of 0 when divided by 4." << "\n";
     cout << "4.  The value of j4 is also 0.0 for the same reason as j3, but the result is stored as a float." << "\n";
     cout << "\n" << "\n";
-    //second at home task
+    //second at home task.
     cout << "ANSWER OF AT HOME ASSIGNMENT no.2:" << "\n";
     cout << "1.  At comment no.4 there is a logic error due to a syntax error as it will do give out the average but it will:" << "\n" << ">first: divide n2 by 2." << "\n" << ">second: add the product of n2/2 to n1." << "\n";
     cout << "2.  At comment no.6 there is a runtime error due to a mathematical error where the program is told to divide by zero in which is not possible therefor it will provide an error." << "\n";
     cout << "3.  All other lines are error free" << "\n";
     cout << "\n" << "\n";
-    //third at home task
+    //third at home task.
     int a=3, b=2, c=-4,y;
     y = a + --b + ++c * a % c ;
     y = 2 * ++b + 3 * ( a-- - c ) ;
@@ -23,6 +23,122 @@ int main(){
     y *= 4* b + 3 * ( ++a + c ) ;
     cout << "ANSWER OF AT HOME ASSIGNMENT no.3:" << "\n";
     cout << "The value of Y is equal to: " << y << "\n";
+    cout << "\n" << "\n";
+    
+    //first at lab assignment.
+    cout << "ANSWER OF AT LAB ASSIGNMENT no.1:" << "\n";
+    for (int z=1; z >=6; ++z){
+        switch (z){
+            case 1:
+            int i=3, j=5, k=7;
+            if (i < j) {
+             if (j < k)
+              i = j;
+              else
+              j = k;
+              }
+              else {
+                if (j > k)
+                j = i;
+                else
+                i = k;
+                }
+                cout << "i = " << i << " j = " << j << " k = " << k << "\n";
+            break;
+            }
+        switch (z){
+            case 2:
+            int i=3, j=7, k=5;
+            if (i < j) {
+             if (j < k)
+              i = j;
+              else
+              j = k;
+              }
+              else {
+                if (j > k)
+                j = i;
+                else
+                i = k;
+                }
+                cout << "i = " << i << " j = " << j << " k = " << k << "\n";
+            break;
+            }
+        switch (z){
+            case 3:
+            int i=5, j=3, k=7;
+            if (i < j) {
+             if (j < k)
+              i = j;
+              else
+              j = k;
+              }
+              else {
+                if (j > k)
+                j = i;
+                else
+                i = k;
+                }
+                cout << "i = " << i << " j = " << j << " k = " << k << "\n";
+            break;
+            }
+        switch (z){
+            case 4:
+            int i=5, j=7, k=3;
+            if (i < j) {
+             if (j < k)
+              i = j;
+              else
+              j = k;
+              }
+              else {
+                if (j > k)
+                j = i;
+                else
+                i = k;
+                }
+                cout << "i = " << i << " j = " << j << " k = " << k << "\n";
+            break;
+            }
+        switch (z){
+            case 5:
+            int i=7, j=3, k=5;
+            if (i < j) {
+             if (j < k)
+              i = j;
+              else
+              j = k;
+              }
+              else {
+                if (j > k)
+                j = i;
+                else
+                i = k;
+                }
+                cout << "i = " << i << " j = " << j << " k = " << k << "\n";
+            break;
+            }
+        switch (z){
+            case 6:
+            int i=7, j=5, k=3;
+            if (i < j) {
+             if (j < k)
+              i = j;
+              else
+              j = k;
+              }
+              else {
+                if (j > k)
+                j = i;
+                else
+                i = k;
+                }
+                cout << "i = " << i << " j = " << j << " k = " << k << "\n";
+            break;
+            }
+    }
+            
+        
 
     //terminal work.
     system("pause");
