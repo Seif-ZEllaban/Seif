effef0f4ecff26b94ee2cce7f4404da8a6bb4185
Seif Zakaria Mohamed Ellaban <62963968+seifzellaban@users.noreply.github.com>
2023-04-07 13:31:31 +0200

    Create Lab 6.cpp

---
+++ Lab 6.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+//Question no.1
+    /*bool isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+}
+    int main() {
+        int x, y;
+        int count = 0;
+        do
+        {
+            do 
+            {
+                cout << "Enter first integers (x < y): ";
+                cin >> x;
+                cout << "Enter second integers (x < y): ";
+                cin >> y;
+            } while (x >= y);
+
+            for (int i = x; i <= y; i++) {
+                if (isPrime(i)) {
+                    count++;
+                }
+            }
+
+            cout << "The number of primes between " << x << " and " << y << " is: " << count << endl << endl;
+        } while (true);
+        system("pause");
+        return 0;
+    }*/
+
+
+//Question no.2
+	/*void main() {
+		float num;
+		int last;
+		do
+		{
+			cout << "Enter a number to view it's times table: ";
+			cin >> num;
+			cout << "Enter an integer number to end the times table: ";
+			cin >> last;
+			cout << "Times Table for " << num << " is: " << endl;
+		
+			for (int i = 0; i <= last; i++) {
+				cout << i << ". " << num << " x " << i << " = " << num * i << endl;
+			}
+			cout << endl;
+		} while (true);
+	}*/
